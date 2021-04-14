a19662f7b7aa48c6de7b4798db68476ebe312071
Merwan-J <76240948+Merwan-J@users.noreply.github.com>
2021-04-14 16:46:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -474,23 +474,23 @@ def main():
     dp.add_handler(MessageHandler(Filters.forwarded,fwd_msg_handler,run_async=True))
     dp.add_handler(CallbackQueryHandler(button,run_async=True))
 
-#     @client.on(events.NewMessage(from_users=(-1001404646371)))
-#     async def my_event_handler(event):
-#         msg = event.message
-#         bot_id = 1633317216
-#         channel_username = 'chillhabesha'
-#         await client.forward_messages(bot_id,msg)
+    @client.on(events.NewMessage(from_users=(-1001404646371)))
+    async def my_event_handler(event):
+        msg = event.message
+        bot_id = 1633317216
+        channel_username = 'chillhabesha'
+        await client.forward_messages(bot_id,msg)
 
    
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
     updater.bot.setWebhook('https://alesera.herokuapp.com/' + TOKEN)
-#     client.start()
-#     print("####################################### client started")
+    client.start()
+    print("####################################### client started")
     
-#     client.run_until_disconnected()
-#     print("####################################### client run until disconected")
+    client.run_until_disconnected()
+    print("####################################### client run until disconected")
     
     updater.idle()
     print("####################################### updater idle")
